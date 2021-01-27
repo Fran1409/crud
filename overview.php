826a2892bf74c09b5650a2de786f91415f170e92
Fran1409 <fran_andries@hotmail.com>
2021-01-27 11:29:01 +0100

    Add form

--- overview.php
+++ overview.php
@@ -5,17 +5,29 @@
 	<meta name="viewport"
 		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Goodcard - track your collection of Pokémon cards</title>
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+
+	<title>Make-up Stash</title>
 </head>
 <body>
 
-<h1>Goodcard - track your collection of Pokémon cards</h1>
+	<h1>Make-up Stash</h1>
 
-<ul>
-<?php foreach ($palettes as $palette) : ?>
-    <li><?= $palette['name'].' - '.$palette['brand'].' - €'.$palette['price'] ?></li>
-<?php endforeach; ?>
-</ul>
+	<ul>
+		<?php foreach ($palettes as $palette) : ?>
+			<li><?= $palette['name'].' - '.$palette['brand'].' - €'.$palette['price'] ?></li>
+		<?php endforeach; ?>
+	</ul>
+
+	<form action="" method="post">
+		<label for="makeup">Make-up Palette: </label>
+		<input type="text" name="makeup"><br>
+		<label for="brand">Brand: </label>
+		<input type="text" name="brand"><br>
+		<label for="price">Price: </label>
+		<input type="text" name="price"><br>
+		<button type="submit" class="btn btn-primary" name="submit">Add to database</button>
+	</form>
 
 </body>
 </html>
